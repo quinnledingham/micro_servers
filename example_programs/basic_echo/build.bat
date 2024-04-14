50bbfba9392c962522a75b6079d2043a90080153
Quinn <quinnledingham@gmail.com>
2024-04-14 15:40:58 -0600

    changing echo build file

--- example_programs/basic_echo/build.bat
+++ example_programs/basic_echo/build.bat
@@ -1,7 +1,13 @@
 @echo off
 
-set CF_DEFAULT= -MD -nologo -Gm- -GR- -EHa- -Od -Oi -FC -Z7 -W3 -EHsc -I./../../
+pushd %CD%
+IF NOT EXIST ../../build mkdir ../../build
+cd ../../build
+
+set CF_DEFAULT= -MD -nologo -Gm- -GR- -EHa- -Od -Oi -FC -Z7 -W3 -EHsc -I./../
 set LF_DEFAULT= -incremental:no -opt:ref -subsystem:console
 
-cl %CF_DEFAULT% -DOS_WINDOWS echo_client.cpp -o ../../build/echo_client /link %LF_DEFAULT%
-cl %CF_DEFAULT% -DOS_WINDOWS echo_server.cpp -o ../../build/echo_server /link %LF_DEFAULT%
+cl %CF_DEFAULT% -DOS_WINDOWS ../example_programs/basic_echo/echo_client.cpp /link %LF_DEFAULT%
+cl %CF_DEFAULT% -DOS_WINDOWS ../example_programs/basic_echo/echo_server.cpp /link %LF_DEFAULT%
+
+cd ../example_programs/basic_echo
